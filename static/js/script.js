375578ad37f7827dddc64882d64fdba21c305ae8
oliviatatum <olivia.tatum1@hotmail.com>
2020-09-30 19:55:54 +0000

    added wireframe images

--- static/js/script.js
+++ static/js/script.js
@@ -8,10 +8,10 @@ $(function ($) {
     $('#addField').on("click",  function(){
         $("#ingredients").append('<textarea class="materialize-textarea" id="meal_ingredients" name="meal_ingredients"/>');
     });
-})
+});
 
 $(function ($) {
     $('#addField2').on("click",  function(){
         $("#instructions").append('<li><textarea id="meal_method" name="meal_method" class="materialize-textarea"></textarea></li>   ');
     });
-})
+});
